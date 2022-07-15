e9cb7257cd89006aec6192f7067712088018f6ff
astridsen <103685822+astridsen@users.noreply.github.com>
2022-07-16 00:04:58 +0200

    Affichage sur la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,199 +1,71 @@
-//Faire le lien entre le produit sur la page d'accueil et la page produit
-
-let str = window.location.href;
-let url = new URL(str);
-let id = url.searchParams.get("id");
-
-console.log(id)
-
-//Éléments à afficher sur la page produit
-const productImg = document.getElementsByClassName(".item__img");
-const productTitle = document.getElementById("title");
-const productPrice = document.getElementById("price");
-const productDescription = document.getElementById("description");
-const productColor = document.getElementById("colors");
-
-fetch('http://localhost:3000/api/products')
-.then(function(res) {
-    if (res.ok) {
-        return res.json();
-    }
-})
-.then(function(product) {
-    productImg.innerHTML = `${product.imageUrl}`
-    productTitle.innerHTml = `${product.name}`
-    productPrice.innerHTML = `${product.price}`
-    productDescription.innerHTML = `${product.description}`
-})
-.catch(function(err) {
-    alert("Erreur produit")
-});
-
-/**Création de la constante items
-const items = document.getElementById("items");
-
-//Récupérer les informations de l'API
-fetch("http://localhost:3000/api/products")
-.then(function(res) {
-    if (res.ok) {
-        return res.json();
-    }
-})
-//Affichage des informations produits
-.then(function(products) {
-    for (let product of products) {
-        items.innerHTML += `<a href="./product.html?id=${product._id}">
-        <article>
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
-        <h3 class="productName">${product.name}</h3>
-        <p class="productDescription">${product.description}</p>
-        </article>   
-        </a>`
-    } ;
-})
-//Affichage d'un message d'erreur si les informations d'un produit ne peuvent pas être réupérés
-.catch(function(err) {
-    alert("Produit indisponible")
-});
+//Récupération de l'ID dans l'url
+const str = window.location.href;
+const url = new URL(str);
 
 
-/**
- * const _productsList = document.getElementById('items');
-
-getProductsFromApi()
-	.then(displayProducts)
-	.catch(displayError);
-
-const getProductsFromApi = () => {
-  return fetch("http://localhost:3000/api/products")
-  	.then((res) => {
-      if (!res.ok) {
-        throw new Error('Error retrieving products!');
-      }
-    
-    	return res.json();
-    })
-};
-
-const displayProducts = (products) => {
-  if (!hasProducts(products) {
-    throw new Error('No products found!');
-  }
-
-	products.forEach((product) => addProductToProductsList(product));
-};
-
-const hasProducts = (products) => {
-  return products.length !== 0;
+const getProductIdFromUrl = (UrlId) => {
+    return new Promise((resolve, reject) => {
+        const id = url.searchParams.get("id");
+        resolve(id);
+        reject(newError('Echec de récupération id produit depuis URL'));
+    });
 }
 
-const addProductToProductsList = (product) => {
-  const _productLink = document.createElement('a');
-  _productLink.href = `./product.html?id=${product._id}`;
-  
-  const _productArticle = document.createElement('article');
-  
-  const _productImage = createImage(product.imageUrl, product.altTxt);
-  
-  
-  // ...
-  
-  _productsList.appendChild(_productLink);
+//Lien entre l'ID récupéré dans l'URL et les informations de l'API
+const getProductFromApi = (productId) => getJsonFromApi(`http://localhost:3000/api/products/${productId}`);
+
+const getJsonFromApi = (url) => {
+    return fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+          throw new Error('Error retrieving products!');
+        }
+        return res.json();
+      })
+        .catch((err) => alert(err));  
 };
+
+//Affichage des informations
+const displayProduct = (product) => {
+    // Affichage de l'image
+    const productImg = createImage(product.imageUrl, product.altTxt);
+    document.querySelector(".item__img").appendChild(productImg);
+
+    // Affichage du titre
+    const productName = document.getElementById('title');
+    productName.textContent = product.name;
+
+    // Affichage du prix
+    const productPrice = document.getElementById('price');
+    productPrice.textContent = product.price;
+
+    // Affichage de la description
+    const productDescription = document.getElementById('description');
+    productDescription.textContent = product.description;
+
+    // Affichage des couleurs
+    for (let colors of product.colors){
+        console.table(colors);
+        let productColors = document.createElement("option");
+        document.querySelector("#colors").appendChild(productColors);
+        productColors.value = colors;
+        productColors.textContent = colors;
+    }
+}
 
 const createImage = (src, alt) => {
-  const _image = document.createElement('img');
-  _img.src = src;
-  _img.alt = alt;
-  
-  return _image;
-};
-
-
-
-
-//Création de la constante items
-const items = document.getElementById("items");
-
-//Récupérer les informations de l'API
-
-.then(function(res) {
-    if (res.ok) {
-        return res.json();
-    }
-})
-//Affichage des informations produits
-.then(function(products) {
-    for (let product of products) {
-        items.innerHTML += `<a href="./product.html?id=${product._id}">
-        <article>
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
-        <h3 class="productName">${product.name}</h3>
-        <p class="productDescription">${product.description}</p>
-        </article>   
-        </a>`
-    } ;
-})
-//Affichage d'un message d'erreur si les informations d'un produit ne peuvent pas être réupérés
-.catch(function(err) {
-    alert("Produit indisponible")
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Product
-
-getProductIdFromUrl()
-	.then(getProductFromApi)
-	.then(displayProduct)
-	.catch(displayError);
-
-
-const getProductIdFromUrl = () => {
-  const url = new UrlParams();
-  
-  return url.get('id');
-};
-
-const getProductFromApi = (productId) => getJsonFromApi(`/api/products?id=${productId}`);
-
-const displayProduct = (product) => {
-  
-  
+    const _image = document.createElement('img');
+    _image.src = src;
+    _image.alt = alt;
+    
+    return _image;
 };
 
 const displayError = (err) => {
-  alert(err);
-};
+    alert("Produit indisponible")
+}
 
-
-
-const getJsonFromApi = (url) => {
-  return fetch(url);
-  	.then((res) => {
-    	if (!res.ok) {
-        throw new Error('...');
-      }
-      
-      return res.json();
-    })
-  	.catch((err) => alert(err));  
-};
-
- */
+getProductIdFromUrl()
+    .then(getProductFromApi)
+    .then(displayProduct)
+    .catch(displayError)
--- front/js/script.js
+++ front/js/script.js
@@ -30,10 +30,10 @@ const addProductToProductsList = (product) => {
   const _productImage = createImage(product.imageUrl, product.altTxt);
   
   const _productName = document.createElement('h3');
-  _productName.innerHTML = `${product.name}`;
+  _productName.textContent = `${product.name}`;
 
   const _productDescription = document.createElement('p');
-  _productDescription.innerHTML = `${product.description}`;
+  _productDescription.textContent = `${product.description}`;
   
   _productsList.appendChild(_productLink);
   _productLink.appendChild(_productArticle);
@@ -42,8 +42,6 @@ const addProductToProductsList = (product) => {
   _productArticle.appendChild(_productDescription);
 
 };
-
-
 
 const createImage = (src, alt) => {
   const _image = document.createElement('img');
